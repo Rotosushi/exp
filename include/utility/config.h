ef843c85cb60afcfb99d366e13cfbd347d7c0e7a
Cade Weinberg <cadeweinberg@gmail.com>
2024-02-15 00:17:49 -0800

    initial project set up

---
+++ include/utility/config.h
@@ -0,0 +1,30 @@
+// Copyright (C) 2024 cade
+//
+// This file is part of exp.
+//
+// exp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// exp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with exp.  If not, see <http://www.gnu.org/licenses/>.
+#pragma once
+// NOLINTBEGIN
+// clang-format off
+#define EXP_VERSION_MAJOR 0
+#define EXP_VERSION_MINOR 0 
+#define EXP_VERSION_PATCH 1
+#define EXP_GIT_REVISION "4d540c4e6f780aac1f86f8eef98f465d9e4b86e0"
+#define EXP_BUILD_DIR "/home/cade/projects/exp/build"
+/* #undef EXP_DEBUG */
+#define EXP_HOST_OS_LINUX
+/* #undef EXP_HOST_OS_WINDOWS */
+/* #undef EXP_HOST_OS_APPLE */
+// NOLINTEND
+// clang-format on
