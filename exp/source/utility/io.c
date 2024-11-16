244ff2086cfe5c15de6611a8d31ca3108e3875f9
Elise <cadeweinberg@gmail.com>
2024-11-15 19:59:00 -0800

    removing C23 extensions from io.c

--- exp/source/utility/io.c
+++ exp/source/utility/io.c
@@ -26,45 +26,45 @@
 #include "utility/panic.h"
 
 FILE *file_open(char const *restrict path, char const *restrict modes) {
-  assert(path != NULL);
-  assert(modes != NULL);
-  FILE *file = fopen(path, modes);
-  if (file == NULL) { PANIC_ERRNO("fopen failed"); }
-  return file;
+    assert(path != NULL);
+    assert(modes != NULL);
+    FILE *file = fopen(path, modes);
+    if (file == NULL) { PANIC_ERRNO("fopen failed"); }
+    return file;
 }
 
 void file_close(FILE *restrict file) {
-  assert(file != NULL);
-  if (fclose(file) == EOF) { PANIC_ERRNO("fclose failed"); }
+    assert(file != NULL);
+    if (fclose(file) == EOF) { PANIC_ERRNO("fclose failed"); }
 }
 
 void file_remove(char const *restrict path) {
-  if (remove(path)) { PANIC_ERRNO("remove failed"); }
+    if (remove(path)) { PANIC_ERRNO("remove failed"); }
 }
 
 void file_write(const char *restrict buffer, FILE *restrict stream) {
-  i32 code = fputs(buffer, stream);
-  if ((code == EOF) && (ferror(stream))) { PANIC_ERRNO("fputs failed"); }
+    i32 code = fputs(buffer, stream);
+    if ((code == EOF) && (ferror(stream))) { PANIC_ERRNO("fputs failed"); }
 }
 
 void file_write_i64(i64 value, FILE *restrict stream) {
-  char buf[i64_safe_strlen(value) + 1] = {};
-  if (i64_to_str(value, buf) == NULL) { PANIC("i64_to_str failed"); }
-  file_write(buf, stream);
+    char buf[i64_safe_strlen(value) + 1];
+    if (i64_to_str(value, buf) == NULL) { PANIC("i64_to_str failed"); }
+    file_write(buf, stream);
 }
 
 void file_write_u64(u64 value, FILE *restrict stream) {
-  char buf[u64_safe_strlen(value) + 1] = {};
-  if (u64_to_str(value, buf) == NULL) { PANIC("u64_to_str failed"); }
-  file_write(buf, stream);
+    char buf[u64_safe_strlen(value) + 1];
+    if (u64_to_str(value, buf) == NULL) { PANIC("u64_to_str failed"); }
+    file_write(buf, stream);
 }
 
 u64 file_read(char *buffer, u64 length, FILE *restrict stream) {
-  u64 count = fread(buffer, sizeof(*buffer), length, stream);
-  int error = ferror(stream);
-  if (error != 0) { PANIC(strerror(error)); }
+    u64 count = fread(buffer, sizeof(*buffer), length, stream);
+    int error = ferror(stream);
+    if (error != 0) { PANIC(strerror(error)); }
 
-  return count;
+    return count;
 }
 
 #if defined(EXP_HOST_SYSTEM_LINUX)
@@ -73,22 +73,22 @@ u64 file_read(char *buffer, u64 length, FILE *restrict stream) {
 #include <unistd.h>
 
 u64 file_length(FILE *restrict file) {
-  i32 fd = fileno(file);
+    i32 fd = fileno(file);
 
-  struct stat info;
-  if (fstat(fd, &info) != 0) { PANIC_ERRNO("fstat failed"); }
+    struct stat info;
+    if (fstat(fd, &info) != 0) { PANIC_ERRNO("fstat failed"); }
 
-  return (u64)info.st_size;
+    return (u64)info.st_size;
 }
 
 #else
 u64 file_length(FILE *restrict file) {
-  if (fseek(file, 0L, SEEK_END) != 0) { PANIC_ERRNO("fseek failed"); }
+    if (fseek(file, 0L, SEEK_END) != 0) { PANIC_ERRNO("fseek failed"); }
 
-  i64 size = ftell(file);
-  if (size == -1L) { PANIC_ERRNO("ftell failed"); }
+    i64 size = ftell(file);
+    if (size == -1L) { PANIC_ERRNO("ftell failed"); }
 
-  rewind(file);
-  return (u64)size;
+    rewind(file);
+    return (u64)size;
 }
 #endif
