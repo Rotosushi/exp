4c718aa9856bcd967a89cc92275ea4b077da9478
Cade Weinberg <cadeweinberg@gmail.com>
2025-05-24 16:39:31 -0700

    fixed(gas_directive_intel_syntax) I had not read the docs close enough, and did not include the "noprefix" option to the .intex_syntax directive, which caused the assembler to throw seemingly incorrect errors on valid assembly.

--- libexp_support/include/support/config.h
+++ libexp_support/include/support/config.h
@@ -21,7 +21,7 @@
 #define EXP_VERSION_MAJOR 0
 #define EXP_VERSION_MINOR 2 
 #define EXP_VERSION_PATCH 0
-#define EXP_GIT_REVISION "2d19553"
+#define EXP_GIT_REVISION "f20bf0b"
 #define EXP_TEST_DIR "/home/cade-weinberg/Source/Projects/exp/test"
 #define EXP_TEST_RESOURCES_DIR "/home/cade-weinberg/Source/Projects/exp/test/resources"
 #define EXP_BINARY_DIR "/home/cade-weinberg/Source/Projects/exp/build"
@@ -32,7 +32,7 @@
 /* #undef EXP_HOST_SYSTEM_APPLE */
 #define EXP_HOST_CPU_x64
 
-#define EXP_VERSION_STRING "exp version (0.2.0) built at (2025-05-22 11:02:43) git revision (2d19553)"
+#define EXP_VERSION_STRING "exp version (0.2.0) built at (2025-05-24 16:18:48) git revision (f20bf0b)"
 // NOLINTEND
 // clang-format on
 
