f90502697ee42dc55f3ab25e9db083694eda50c8
Cade Weinberg <cadeweinberg@gmail.com>
2026-01-23 07:15:45 -0800

    removed extraneous 'support' library
    The library was factored out for no good reason. it complicates the
    build process to have it separated, and this should result in less
    work overall.

--- exp/include/support/config.h
+++ exp/include/support/config.h
@@ -21,7 +21,7 @@
 #define EXP_VERSION_MAJOR 0
 #define EXP_VERSION_MINOR 2 
 #define EXP_VERSION_PATCH 0
-#define EXP_GIT_REVISION "706ea09"
+#define EXP_GIT_REVISION "7e18e6f"
 #define EXP_TEST_DIR "/home/cadence/Projects/exp/test"
 #define EXP_TEST_RESOURCES_DIR "/home/cadence/Projects/exp/test/resources"
 #define EXP_BINARY_DIR "/home/cadence/Projects/exp/build"
@@ -32,7 +32,7 @@
 /* #undef EXP_HOST_SYSTEM_APPLE */
 #define EXP_HOST_CPU_x64
 
-#define EXP_VERSION_STRING "exp version (0.2.0) built at (2026-01-19 15:56:10) git revision (706ea09)"
+#define EXP_VERSION_STRING "exp version (0.2.0) built at (2026-01-23 07:04:03) git revision (7e18e6f)"
 // NOLINTEND
 // clang-format on
 
