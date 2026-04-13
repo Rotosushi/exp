5482409db8179a5c802588eb99375161bcadf32c
Cade Weinberg <cadeweinberg@gmail.com>
2026-04-13 12:58:57 -0700

    beginning of new refactor path

--- libexp_support/include/support/config.h
+++ libexp_support/include/support/config.h
@@ -21,18 +21,18 @@
 #define EXP_VERSION_MAJOR 0
 #define EXP_VERSION_MINOR 2 
 #define EXP_VERSION_PATCH 0
-#define EXP_GIT_REVISION "a3e4c64"
-#define EXP_TEST_DIR "/home/cade-weinberg/Source/Projects/exp/test"
-#define EXP_TEST_RESOURCES_DIR "/home/cade-weinberg/Source/Projects/exp/test/resources"
-#define EXP_BINARY_DIR "/home/cade-weinberg/Source/Projects/exp/build"
+#define EXP_GIT_REVISION "26eb113"
+#define EXP_TEST_DIR "/home/cade-weinberg/Projects/Programming/Compilers/exp/test"
+#define EXP_TEST_RESOURCES_DIR "/home/cade-weinberg/Projects/Programming/Compilers/exp/test/resources"
+#define EXP_BINARY_DIR "/home/cade-weinberg/Projects/Programming/Compilers/exp/build"
 #define EXP_LIBEXP_RUNTIME_SOURCE_DIR ""
-#define EXP_LIBEXP_RUNTIME_BINARY_DIR "/home/cade-weinberg/Source/Projects/exp/build/libexp_runtime"
+#define EXP_LIBEXP_RUNTIME_BINARY_DIR "/home/cade-weinberg/Projects/Programming/Compilers/exp/build/libexp_runtime"
 #define EXP_HOST_SYSTEM_LINUX
 /* #undef EXP_HOST_SYSTEM_WINDOWS */
 /* #undef EXP_HOST_SYSTEM_APPLE */
 #define EXP_HOST_CPU_x64
 
-#define EXP_VERSION_STRING "exp version (0.2.0) built at (2025-04-28 09:11:40) git revision (a3e4c64)"
+#define EXP_VERSION_STRING "exp version (0.2.0) built at (2026-04-13 12:55:16) git revision (26eb113)"
 // NOLINTEND
 // clang-format on
 
