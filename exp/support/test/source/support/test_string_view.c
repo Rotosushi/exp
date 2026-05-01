1a1e2e45a604991d7c8ee2c4e2c522778190a96d
Cade Weinberg <cadeweinberg@gmail.com>
2026-05-01 11:35:29 -0700

    test: string view test coverage

--- exp/support/test/source/support/test_string_view.c
+++ exp/support/test/source/support/test_string_view.c
@@ -10,6 +10,71 @@ START_TEST(test_string_view) {
 }
 END_TEST
 
+START_TEST(test_string_view_create) {
+    StringView view =
+        string_view_create("Hello, World", sizeof("Hello, World") - 1);
+    ck_assert_int_eq(view.length, sizeof("Hello, World") - 1);
+    ck_assert_str_eq(view.data, "Hello, World");
+}
+END_TEST
+
+START_TEST(test_string_view_from_cstr) {
+    StringView view = string_view_from_cstr("Hello, World");
+    ck_assert_int_eq(view.length, sizeof("Hello, World") - 1);
+    ck_assert_str_eq(view.data, "Hello, World");
+}
+END_TEST
+
+START_TEST(test_string_view_length) {
+    StringView view = SV("Hello");
+    ck_assert_int_eq(string_view_length(view), 5);
+}
+END_TEST
+
+START_TEST(test_string_view_cstr) {
+    StringView view = SV("Hello");
+    ck_assert_str_eq(string_view_cstr(view), "Hello");
+}
+END_TEST
+
+START_TEST(test_string_view_cmp) {
+    StringView a = SV("Hello");
+    StringView b = SV("Hello");
+    StringView c = SV("World");
+
+    ck_assert_int_eq(string_view_cmp(a, b), 0);
+    ck_assert_int_lt(string_view_cmp(a, c), 0);
+    ck_assert_int_gt(string_view_cmp(c, a), 0);
+}
+END_TEST
+
+START_TEST(test_string_view_cmp_cstr) {
+    StringView view = SV("Hello");
+
+    ck_assert_int_eq(string_view_cmp_cstr(view, "Hello"), 0);
+    ck_assert_int_lt(string_view_cmp_cstr(view, "World"), 0);
+    ck_assert_int_gt(string_view_cmp_cstr(SV("World"), "Hello"), 0);
+}
+END_TEST
+
+START_TEST(test_string_view_eq) {
+    StringView a = SV("Hello");
+    StringView b = SV("Hello");
+    StringView c = SV("World");
+
+    ck_assert(string_view_eq(a, b));
+    ck_assert(!string_view_eq(a, c));
+}
+END_TEST
+
+START_TEST(test_string_view_eq_cstr) {
+    StringView view = SV("Hello");
+
+    ck_assert(string_view_eq_cstr(view, "Hello"));
+    ck_assert(!string_view_eq_cstr(view, "World"));
+}
+END_TEST
+
 Suite *test_string_view_suite(void) {
     Suite *s = suite_create("string_view");
 
@@ -17,11 +82,51 @@ Suite *test_string_view_suite(void) {
     tcase_add_test(tc_core, test_string_view);
     suite_add_tcase(s, tc_core);
 
+    tc_core = tcase_create("exp.support.StringView.create");
+    tcase_add_test(tc_core, test_string_view_create);
+    suite_add_tcase(s, tc_core);
+
+    tc_core = tcase_create("exp.support.StringView.from_cstr");
+    tcase_add_test(tc_core, test_string_view_from_cstr);
+    suite_add_tcase(s, tc_core);
+
+    tc_core = tcase_create("exp.support.StringView.length");
+    tcase_add_test(tc_core, test_string_view_length);
+    suite_add_tcase(s, tc_core);
+
+    tc_core = tcase_create("exp.support.StringView.cstr");
+    tcase_add_test(tc_core, test_string_view_cstr);
+    suite_add_tcase(s, tc_core);
+
+    tc_core = tcase_create("exp.support.StringView.cmp");
+    tcase_add_test(tc_core, test_string_view_cmp);
+    suite_add_tcase(s, tc_core);
+
+    tc_core = tcase_create("exp.support.StringView.cmp_cstr");
+    tcase_add_test(tc_core, test_string_view_cmp_cstr);
+    suite_add_tcase(s, tc_core);
+
+    tc_core = tcase_create("exp.support.StringView.eq");
+    tcase_add_test(tc_core, test_string_view_eq);
+    suite_add_tcase(s, tc_core);
+
+    tc_core = tcase_create("exp.support.StringView.eq_cstr");
+    tcase_add_test(tc_core, test_string_view_eq_cstr);
+    suite_add_tcase(s, tc_core);
+
     return s;
 }
 
 char *const test_string_view_names[] = {
-    "test_string_view",
+    "exp.support.StringView",
+    "exp.support.StringView.create",
+    "exp.support.StringView.from_cstr",
+    "exp.support.StringView.length",
+    "exp.support.StringView.cstr",
+    "exp.support.StringView.cmp",
+    "exp.support.StringView.cmp_cstr",
+    "exp.support.StringView.eq",
+    "exp.support.StringView.eq_cstr",
 };
 size_t const test_string_view_names_count =
     sizeof(test_string_view_names) / sizeof(test_string_view_names[0]);
